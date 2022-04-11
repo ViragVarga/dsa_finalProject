b64b15a9762728071d902574a3533f4f048c76af
ViragVarga <varga.virag.jenny@gmail.com>
2022-04-11 15:02:00 +0100

    Part_2 is finished

--- final_project/final_project/FinalProject.java
+++ final_project/final_project/FinalProject.java
@@ -1,12 +1,113 @@
 package final_project;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import edu.princeton.cs.algs4.*;
 
 public class FinalProject {
 	
-	
-	
 	public static void main(String[] args) {
+		ArrayList<String> stopLines = new ArrayList<String>();
+		boolean fileRead = false;
+		try(BufferedReader in = new BufferedReader(new FileReader("stops.txt"))) {
+		    String str;
+		    while ((str = in.readLine()) != null) {
+		    	stopLines.add(str);
+		    }
+		    fileRead = true;
+		}
+		catch (Exception e) {
+		    System.out.println("File read-in failed! Error message: " + e);
+		    fileRead = false;
+		}
+		String[] stopArray = new String[stopLines.size()-1];
+		for(int i=1; i<stopLines.size(); i++)
+			stopArray[i-1] = stopLines.get(i);
+		
+		if(fileRead) {
+			stop stops[] = new stop[stopArray.length];
+			try {
+				for(int i=0; i<stopArray.length; i++) {
+					stops[i] = new stop();
+					String[] data = stopArray[i].split(",");
+					int counter = 0;
+					
+					stops[i].setID(Integer.parseInt(data[counter]));
+					counter++;
+					if(data[counter].equals(" "))
+						stops[i].setCode(-1);
+					else
+						stops[i].setCode(Integer.parseInt(data[counter]));
+					counter++;
+					stops[i].setName(data[counter]);
+					counter++;
+					stops[i].setDesc(data[counter]);
+					counter++;
+					stops[i].setCoordinates(Double.parseDouble(data[counter]), Double.parseDouble(data[counter+1]));
+					counter += 2;
+					stops[i].setZoneID(data[counter]);
+					counter++;
+					stops[i].setURL(data[counter]);
+					counter++;
+					stops[i].setLocationType(data[counter]);
+					counter++;
+					if(counter >= data.length) 
+						stops[i].setParentLocation("");
+					else
+						stops[i].setParentLocation(data[counter]);
+				}
+			}
+			catch (Exception e) {
+					System.out.println("Couldn't set data of the stops. Error message: " + e);
+			}
+			
+				boolean exit = false;
+				TST<Integer> tst = new TST<Integer>();
+				for(stop i : stops) {
+					String name[] = i.getName().split(" ");
+					String key = "";
+					if(name[0].equals("FLAGSTOP") || name[0].equals("EB") || name[0].equals("NB")
+							|| name[0].equals("SB") || name[0].equals("WB")) {
+						for(int s = 1; s<name.length; s++) {
+							key += name[s] + " ";
+						}
+						key += name[0];
+					}
+					else {
+						key = i.getName();
+					}
+//					System.out.println(key);
+					
+					int id = i.getID();
+					
+					tst.put(key, id);
+//					trie.add(key, id);
+				
+				}
+
+				stop.sortStops(stops);
+				Scanner scanner = new Scanner(System.in);
+				while(!exit) {
+					System.out.println("Please enter stop name (or type \"exit\" to stop): ");
+					String inSearch = scanner.nextLine();
+					if(inSearch.equalsIgnoreCase("exit")) {
+						exit = true;
+					}
+					else {
+						Iterable<String> searchRes = tst.keysWithPrefix(inSearch.toUpperCase());
+						for(String i : searchRes) {
+							stop tmp = stop.findStop(stops, i);
+							tmp.displayInfo();
+						}
+					}
+				
+				}
+				scanner.close();
+		}
+		
 		
 	}
 
--- final_project/final_project/stop.java
+++ final_project/final_project/stop.java
@@ -2,7 +2,8 @@ package final_project;
 
 public class stop {
 	
-	private int ID, code;
+	private int code;
+	private Integer ID;
 	private String name, desc, zoneID, url, loc_type, parent_station;
 	private double lat, lon; 
 		
@@ -83,5 +84,64 @@ public class stop {
 				"\nURL: " +this.url+
 				"\nLocation type: " + this.loc_type+
 				"\nParent station: "+this.parent_station+"\n\n");
+	}
+	
+	public static void sortStops(stop[] a) {
+		stop[] tmp = new stop[a.length];
+		sort(a, tmp, 0, a.length-1);
+	}
+	private static void sort(stop[] a, stop[] tmp, int lo, int hi) {
+		if(hi <= lo) 
+			return;
+		int mid = lo + (hi-lo) /2;
+		sort(a, tmp, lo, mid);
+		sort(a, tmp, mid+1, hi);
+		merge(a, tmp, lo, mid, hi);
+	}
+	private static void merge(stop[] a, stop[] tmp, int lo, int mid, int hi) {
+		for(int k = lo; k <= hi; k++)
+			tmp[k] = a[k];
+		
+		int i = lo, j = mid+1;
+		for(int k = lo; k <= hi; k++) {
+			if(i > mid)
+				a[k] = tmp[j++];
+			else if(j > hi)
+				a[k] = tmp[i++];
+			else if(tmp[j].getID() < tmp[i].getID())
+				a[k] = tmp[j++];
+			else
+				a[k] = tmp[i++];
+		}
+	}
+	
+	public static stop findStop(stop[] a, int id) {
+		int lo = 0, hi = a.length-1;
+		while(lo <= hi) {
+			int mid = lo + (hi - lo) / 2;
+			if(id < a[mid].getID())
+				hi = mid - 1;
+			else if(id > a[mid].getID())
+				lo = mid + 1;
+			else
+				return a[mid];
+		}
+		return null;
+	}
+	
+	public static stop findStop(stop[] a, String name) {
+		String[] s = name.split(" ");
+		if(s[s.length-1].equalsIgnoreCase("flagstop") || s[s.length-1].equalsIgnoreCase("eb") || 
+				s[s.length-1].equalsIgnoreCase("wb") || s[s.length-1].equalsIgnoreCase("sb") || s[s.length-1].equalsIgnoreCase("nb")) {
+			name = s[s.length-1];
+			for(int i=0; i<s.length-1; i++) {
+				name += " " + s[i];
+			}
+		}
+		for(stop i : a) {
+			if(i.getName().compareToIgnoreCase(name) == 0)
+				return i;
+		}
+		return null;
 	}
 }
